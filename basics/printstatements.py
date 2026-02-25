1ec284f94c0ed194f1994edd437ef6a2370e7c1e
PrasannavenkateshM <prasanna4630@gmail.com>
2026-02-25 22:58:54 +0530

    added new files as of day-1

---
+++ basics/printstatements.py
@@ -0,0 +1,14 @@
+print("Hey!, This is the first time I am trying to write a code. I am in attending the client call and trying to learn")
+
+name = "Prasannavenkatesh Manigandan"
+print(name)
+age = 12+10
+print(age)
+intern_experience=0.6
+work_experience=0.8
+print("Total work experience", intern_experience+work_experience)
+
+name_input=input("Enter your name:")
+age_input=input("Enter your age:")
+workexp_input=float(input("Enter your total work experience:"))
+print(name_input,age_input,workexp_input, sep="\n")
